e7fc68eae3871ebb7d16f1bfcf47c1a8d5a812c7
ulrichtoussom <ulrichtoussom@gmail.com>
2026-02-10 20:36:22 +0100

    Ajout et transformation de l'arg NEXT_PUBLIC_SITEURL dans DockerFile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+  // Cela aide Next.js à comprendre qu'il est derrière un proxy (Render)
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'chaweb.onrender.com'],
+    },
+  },
+}
